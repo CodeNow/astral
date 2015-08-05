e45734c5a6079f27768a3f3c3396faa0caabd0e8
Ryan Sandor Richards <ryan@runnable.com>
2015-08-05 13:47:17 -0700

    Added promise rejection method to the error module.

--- test/unit/error.js
+++ test/unit/error.js
@@ -25,10 +25,41 @@ describe('Error', function() {
     done();
   });
 
-  it('should use the internal logger when logging errors', function(done) {
-    var err = new Error('this is error!!! (like sparta)');
-    error.log(err);
-    expect(logger.error.calledWith(err)).to.be.true();
-    done();
+  describe('log', function() {
+    it('should use the internal logger when logging errors', function(done) {
+      var err = new Error('this is error!!! (like sparta)');
+      error.log(err);
+      expect(logger.error.calledWith(err)).to.be.true();
+      done();
+    });
+  });
+
+  describe('reject', function() {
+    beforeEach(function (done) {
+      sinon.spy(error, 'create');
+      done();
+    });
+
+    afterEach(function (done) {
+      error.create.restore();
+      done();
+    });
+
+    it('should return a rejection promise', function (done) {
+      error.reject(500, 'server error').catch(function (err) {
+        expect(err).to.exist();
+        done();
+      });
+    });
+
+    it('should use `.create` to create the error', function(done) {
+      var code = 500;
+      var message = 'this is errorlandwow';
+      var data = { a: 20 };
+      error.reject(code, message, data).catch(function (err) {
+        expect(error.create.calledWith(code, message, data)).to.be.true();
+        done();
+      });
+    });
   });
 });
