60112384638e2b8b61420dcf24dc86fe1d2d494d
Ryan Sandor Richards <ryan@runnable.com>
2015-08-18 19:48:18 -0700

    Finished index and added start script.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 'use strict';
 
+var server = require('./lib/server');
+var error = require('./lib/error');
+var log = require('./lib/logger');
+
 /**
  * Entrypoint for the shiva provisioning manager.
  * @author Ryan Sandor Richards
  * @module shiva
  */
+
+server.start(function (err) {
+  if (err) {
+    var message = 'Failed to start shiva worker server';
+    error.wrapAndReport(err, 500, message);
+    logger.fatal({ err: err }, message);
+    return;
+  }
+});
