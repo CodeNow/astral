9074484e478b7558946b90db2e543f0a6dc92618
Ryan Sandor Richards <ryan@runnable.com>
2015-08-24 12:41:34 -0700

    Final fixes for correct AMI deploy on beta.

--- scripts/aws-instance-user-data.sh
+++ scripts/aws-instance-user-data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ENV_FILE=/etc/environment
+ENV_FILE=/opt/runnable/env
 HOST_TAGS_FILE=/opt/runnable/host_tags
 DOCK_INIT_SCRIPT=/opt/runnable/dock-init/init.sh
 
 # Set preferred versions of each of the dock services
-echo 'FILIBUSTER_VERSION={{filibuster_version}}' >> $ENV_FILE
-echo 'KRAIN_VERSION={{krain_version}}' >> $ENV_FILE
-echo 'SAURON_VERSION={{sauron_version}}' >> $ENV_FILE
-echo 'IMAGE_BUILDER_VERSION={{image_builder_version}}' >> $ENV_FILE
-echo 'DOCKER_LISTENER_VERSION={{docker_listener_version}}' >> $ENV_FILE
+echo 'export FILIBUSTER_VERSION={{filibuster_version}}' >> $ENV_FILE
+echo 'export KRAIN_VERSION={{krain_version}}' >> $ENV_FILE
+echo 'export SAURON_VERSION={{sauron_version}}' >> $ENV_FILE
+echo 'export IMAGE_BUILDER_VERSION={{image_builder_version}}' >> $ENV_FILE
+echo 'export DOCKER_LISTENER_VERSION={{docker_listener_version}}' >> $ENV_FILE
 
 # Set the host tags file (used by upstart for docker-listener)
 echo '{{host_tags}}' > $HOST_TAGS_FILE
