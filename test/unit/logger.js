dbed0c623665e8644e622ea71a844c05c2b056f9
Ryan Sandor Richards <ryan@runnable.com>
2015-08-27 15:31:09 -0700

    Added better job logging.

--- test/unit/logger.js
+++ test/unit/logger.js
@@ -45,5 +45,58 @@ describe('logger', function() {
         done();
       });
     }); // end 'err'
+
+    describe('job', function() {
+      it('should serialize non-instance fields', function(done) {
+        var job = { a: 'a', b: 'b', c: 'foo' };
+        expect(logger.serializers.job(job)).to.deep.equal(job);
+        done();
+      });
+
+      it('should limit `instances` to specific fields', function(done) {
+        var job = {
+          instances: [
+            {
+              ImageId: 'image-id',
+              InstanceId: 'instance-id',
+              InstanceType: 'instance-type',
+              KeyName: 'key-name',
+              PrivateIpAddress: 'private-ip-address',
+              SubnetId: 'subnet-id',
+              VpcId: 'vpc-id',
+              NotThere: 'woo',
+              TotesNotThere: {
+                Nope: 'hahaha'
+              }
+            }
+          ]
+        };
+
+        var expectedFields = [
+          'ImageId', 'InstanceId', 'InstanceType', 'KeyName',
+          'PrivateIpAddress', 'SubnetId', 'VpcId'
+        ];
+        var unexpectedFields = [
+          'NotThere', 'TotesNotThere'
+        ];
+
+        var result = logger.serializers.job(job);
+        expectedFields.forEach(function (field) {
+          expect(result.instances[0][field], field)
+            .to.equal(job.instances[0][field]);
+        });
+        unexpectedFields.forEach(function (field) {
+          expect(result.instances[0][field]).to.not.exist();
+        });
+
+        done();
+      });
+
+      it('should serialize non-array `instances` directly', function(done) {
+        var job = { instances: 'not-an-array' };
+        expect(logger.serializers.job(job).instances).to.equal(job.instances);
+        done();
+      });
+    }); // end 'job'
   }); // end 'serializers'
 }); // end 'logger'
