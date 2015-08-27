75b970a01c4b0b62beaa339e1702e73ea6c946a1
Ryan Sandor Richards <ryan@runnable.com>
2015-08-27 16:49:01 -0700

    Reworked `clusters` and `instances` tables.
    - `clusters` table now has specific 3rd party organization id fields (currently only `github_id`).
    - Removed superflous AWS specific configuration from clusters `clusters` (see migration for full explanation: `20150827155904_remove_redundant_cluster_fields.js`)
    - `instances` now prefixes AWS fields with `aws` (except for `id`)
    - Renamed `instances.type` to `instances.role`. We are not going to differentiate between EC2 instances based on type of dock anymore. For now there is simply one type: `dock`. In the future we can add different types of EC2 instances (such as `services`)

---
+++ migrations/20150827161444_add_cluster_github_id_column.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var debug = require('debug')('shiva:migration');
+
+/**
+ * Adds a new field named `github_id` to the clusters table that holds the id
+ * of the github user/organization associated with the cluster.
+ *
+ * @author Ryan Sandor Richards
+ */
+
+exports.up = function(knex, Promise) {
+  var updateTable = knex.schema.table('clusters', function (table) {
+    table.string('github_id', 36).unique().notNullable();
+  });
+  debug(updateTable.toString());
+  return updateTable;
+};
+
+exports.down = function(knex, Promise) {
+  var updateTable = knex.schema.table('clusters', function (table) {
+    table.dropColumn('github_id');
+  });
+  debug(updateTable.toString());
+  return updateTable;
+};
