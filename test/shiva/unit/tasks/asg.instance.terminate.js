4df7b481f2bee1bafd14423e6b28a4989fe81f25
Ryan Sandor Richards <ryan@runnable.com>
2015-12-18 16:40:55 -0800

    asg.instance.terminate now uses the new EC2 sdk.

--- test/shiva/unit/tasks/asg.instance.terminate.js
+++ test/shiva/unit/tasks/asg.instance.terminate.js
@@ -18,7 +18,7 @@ var Promise = require('bluebird');
 var TaskError = require('ponos').TaskError;
 var TaskFatalError = require('ponos').TaskFatalError;
 
-var aws = require(process.env.ASTRAL_ROOT + 'shiva/aws');
+var ec2 = require(process.env.ASTRAL_ROOT + 'shiva/models/aws/ec2');
 var asgInstanceTerminate = require(
   process.env.ASTRAL_ROOT +
   'shiva/tasks/asg.instance.terminate'
@@ -30,7 +30,7 @@ describe('shiva', function() {
       var ipInstanceId = 'instance-id-for-ip';
 
       beforeEach(function (done) {
-        sinon.stub(aws, 'describeInstances').returns(Promise.resolve({
+        sinon.stub(ec2, 'describeInstancesAsync').returns(Promise.resolve({
           Reservations: [
             {
               Instances: [
@@ -39,13 +39,13 @@ describe('shiva', function() {
             }
           ]
         }));
-        sinon.stub(aws, 'terminateInstances').returns(Promise.resolve());
+        sinon.stub(ec2, 'terminateInstancesAsync').returns(Promise.resolve());
         done();
       });
 
       afterEach(function (done) {
-        aws.terminateInstances.restore();
-        aws.describeInstances.restore();
+        ec2.terminateInstancesAsync.restore();
+        ec2.describeInstancesAsync.restore();
         done();
       });
 
@@ -80,7 +80,7 @@ describe('shiva', function() {
           var job = { instanceId: 'some-id' };
           asgInstanceTerminate(job).asCallback(function (err) {
             expect(err).to.not.exist();
-            expect(aws.describeInstances.callCount).to.equal(0);
+            expect(ec2.describeInstancesAsync.callCount).to.equal(0);
             done();
           });
         });
@@ -90,7 +90,7 @@ describe('shiva', function() {
           var job = { instanceId: instanceId };
           asgInstanceTerminate(job).asCallback(function (err) {
             expect(err).to.not.exist();
-            expect(aws.terminateInstances.firstCall.args[0]).to.deep.equal({
+            expect(ec2.terminateInstancesAsync.firstCall.args[0]).to.deep.equal({
               InstanceIds: [ instanceId ]
             });
             done();
@@ -116,15 +116,15 @@ describe('shiva', function() {
           };
           asgInstanceTerminate(job).asCallback(function (err) {
             expect(err).to.not.exist();
-            expect(aws.describeInstances.calledOnce).to.be.true();
-            expect(aws.describeInstances.firstCall.args[0])
+            expect(ec2.describeInstancesAsync.calledOnce).to.be.true();
+            expect(ec2.describeInstancesAsync.firstCall.args[0])
               .to.deep.equal(expectedQuery);
             done();
           });
         });
 
         it('should fatally reject if no instance has the given `ipAddress`', function (done) {
-          aws.describeInstances.returns(Promise.resolve({ Reservations: [] }));
+          ec2.describeInstancesAsync.returns(Promise.resolve({ Reservations: [] }));
           var job = { ipAddress: '1.2.3.4' };
           asgInstanceTerminate(job).asCallback(function (err) {
             expect(err).to.be.an.instanceof(TaskFatalError);
@@ -137,8 +137,8 @@ describe('shiva', function() {
           var job = { ipAddress: '0.1.0.1' };
           asgInstanceTerminate(job).asCallback(function (err) {
             expect(err).to.not.exist();
-            expect(aws.terminateInstances.calledOnce).to.be.true();
-            expect(aws.terminateInstances.firstCall.args[0]).to.deep.equal({
+            expect(ec2.terminateInstancesAsync.calledOnce).to.be.true();
+            expect(ec2.terminateInstancesAsync.firstCall.args[0]).to.deep.equal({
               InstanceIds: [ipInstanceId]
             });
             done();
@@ -149,7 +149,7 @@ describe('shiva', function() {
       it('should fatally error if the instance does not exist', function (done) {
         var awsError = new Error('No instance found');
         awsError.code = 'InvalidInstanceID.NotFound';
-        aws.terminateInstances.returns(Promise.reject(awsError));
+        ec2.terminateInstancesAsync.returns(Promise.reject(awsError));
         var job = { instanceId: '2345' };
         asgInstanceTerminate(job).asCallback(function (err) {
           expect(err).to.be.an.instanceof(TaskFatalError);
@@ -160,7 +160,7 @@ describe('shiva', function() {
 
       it('should re-throw unknown errors', function (done) {
         var randomError = new Error('What? I mean keewl!');
-        aws.terminateInstances.returns(Promise.reject(randomError));
+        ec2.terminateInstancesAsync.returns(Promise.reject(randomError));
         var job = { instanceId: '123444' };
         asgInstanceTerminate(job).asCallback(function (err) {
           expect(err).to.equal(randomError);
