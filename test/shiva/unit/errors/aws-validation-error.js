041e692deeecf751583476320a53e5210d6f3f18
Ryan Sandor Richards <ryan@runnable.com>
2016-01-08 12:03:39 -0800

    Added validation error casting for aws related models.

---
+++ test/shiva/unit/errors/aws-validation-error.js
@@ -0,0 +1,42 @@
+'use strict';
+
+'use strict';
+
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var before = lab.before;
+var after = lab.after;
+var beforeEach = lab.beforeEach;
+var afterEach = lab.afterEach;
+var Code = require('code');
+var expect = Code.expect;
+var sinon = require('sinon');
+
+var astralRequire = require(process.env.ASTRAL_ROOT + '../test/fixtures/astral-require');
+var loadenv = require('loadenv');
+loadenv.restore();
+loadenv({ project: 'shiva', debugName: 'astral:shiva:test' });
+
+var TaskFatalError = require('ponos').TaskFatalError;
+var AWSValidationError = astralRequire('shiva/errors/aws-validation-error');
+
+describe('shiva', function() {
+  describe('errors', function() {
+    describe('AWSValidationError', function() {
+      it('should extend TaskFatalError', function(done) {
+        var err = new AWSValidationError(new Error('WOW'));
+        expect(err).to.be.an.instanceof(TaskFatalError);
+        done();
+      });
+
+      it('should set the message of the given error', function(done) {
+        var msg = 'This is an error message';
+        var err = new AWSValidationError(new Error(msg));
+        expect(err.message).to.equal(msg);
+        done();
+      });
+    }); // end 'AWSAlreadyExistsError'
+  }); // end 'errors'
+}); // end 'shiva'
