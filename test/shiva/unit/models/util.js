0a08f84c6201e983987bd89600232e560b0515ce
Anandkumar Patel <anand@runnable.com>
2016-10-20 11:51:59 -0700

    add already attached error

--- test/shiva/unit/models/util.js
+++ test/shiva/unit/models/util.js
@@ -1,75 +1,84 @@
 'use strict'
+const astralRequire = require('../../../../test/fixtures/astral-require')
+const Code = require('code')
+const Lab = require('lab')
+const loadenv = require('loadenv')
+require('sinon-as-promised')(require('bluebird'))
 
-var Lab = require('lab')
-var lab = exports.lab = Lab.script()
-var describe = lab.describe
-var it = lab.it
-var Code = require('code')
-var expect = Code.expect
-
-var astralRequire = require(process.env.ASTRAL_ROOT + '../test/fixtures/astral-require')
-var loadenv = require('loadenv')
+const AWSAlreadyExistsError = astralRequire('shiva/errors/aws-already-exists-error')
+const AWSAlreadyPartOfASGError = require('shiva/errors/aws-already-part-of-asg-error')
+const AWSInvalidParameterTypeError = astralRequire('shiva/errors/aws-invalid-parameter-type-error')
+const AWSRateLimitError = astralRequire('shiva/errors/aws-rate-limit-error')
+const AWSValidationError = astralRequire('shiva/errors/aws-validation-error')
+const lab = exports.lab = Lab.script()
+const Util = astralRequire('shiva/models/util')
 loadenv.restore()
+
+const describe = lab.describe
+const expect = Code.expect
+const it = lab.it
 loadenv({ project: 'shiva', debugName: 'astral:shiva:test' })
 
-var Util = astralRequire('shiva/models/util')
-var AWSAlreadyExistsError = astralRequire('shiva/errors/aws-already-exists-error')
-var AWSInvalidParameterTypeError = astralRequire('shiva/errors/aws-invalid-parameter-type-error')
-var AWSRateLimitError = astralRequire('shiva/errors/aws-rate-limit-error')
-var AWSValidationError = astralRequire('shiva/errors/aws-validation-error')
+describe('shiva/models/utils unit test', function () {
+  describe('castAWSError', function () {
+    it('should cast AWSAlreadyExistsError', function (done) {
+      const awsError = new Error()
+      awsError.code = 'AlreadyExists'
+      try {
+        Util.castAWSError(awsError)
+      } catch (err) {
+        expect(err).to.be.an.instanceof(AWSAlreadyExistsError)
+        expect(err.data.originalError).to.equal(awsError)
+        done()
+      }
+    })
 
-describe('shiva', function () {
-  describe('models', function () {
-    describe('Util', function () {
-      describe('castAWSError', function () {
-        it('should cast AWSAlreadyExistsError', function (done) {
-          var awsError = new Error()
-          awsError.code = 'AlreadyExists'
-          try {
-            Util.castAWSError(awsError)
-          } catch (err) {
-            expect(err).to.be.an.instanceof(AWSAlreadyExistsError)
-            expect(err.data.originalError).to.equal(awsError)
-            done()
-          }
-        })
+    it('should cast AWSValidationError', function (done) {
+      const awsError = new Error()
+      awsError.code = 'ValidationError'
+      try {
+        Util.castAWSError(awsError)
+      } catch (err) {
+        expect(err).to.be.an.instanceof(AWSValidationError)
+        expect(err.data.originalError).to.equal(awsError)
+        done()
+      }
+    })
 
-        it('should cast AWSValidationError', function (done) {
-          var awsError = new Error()
-          awsError.code = 'ValidationError'
-          try {
-            Util.castAWSError(awsError)
-          } catch (err) {
-            expect(err).to.be.an.instanceof(AWSValidationError)
-            expect(err.data.originalError).to.equal(awsError)
-            done()
-          }
-        })
+    it('should cast AWSRateLimitError', function (done) {
+      const awsError = new Error('EC2 Request limit exceeded.')
+      awsError.code = 'ValidationError'
+      try {
+        Util.castAWSError(awsError)
+      } catch (err) {
+        expect(err).to.be.an.instanceof(AWSRateLimitError)
+        expect(err.data.originalError).to.equal(awsError)
+        done()
+      }
+    })
 
-        it('should cast AWSRateLimitError', function (done) {
-          var awsError = new Error('EC2 Request limit exceeded.')
-          awsError.code = 'ValidationError'
-          try {
-            Util.castAWSError(awsError)
-          } catch (err) {
-            expect(err).to.be.an.instanceof(AWSRateLimitError)
-            expect(err.data.originalError).to.equal(awsError)
-            done()
-          }
-        })
+    it('should cast AWSInvalidParameterTypeError', function (done) {
+      const awsError = new Error()
+      awsError.code = 'InvalidParameterType'
+      try {
+        Util.castAWSError(awsError)
+      } catch (err) {
+        expect(err).to.be.an.instanceof(AWSInvalidParameterTypeError)
+        expect(err.data.originalError).to.equal(awsError)
+        done()
+      }
+    })
 
-        it('should cast AWSInvalidParameterTypeError', function (done) {
-          var awsError = new Error()
-          awsError.code = 'InvalidParameterType'
-          try {
-            Util.castAWSError(awsError)
-          } catch (err) {
-            expect(err).to.be.an.instanceof(AWSInvalidParameterTypeError)
-            expect(err.data.originalError).to.equal(awsError)
-            done()
-          }
-        })
-      }) // end'castAWSError
-    }) // end 'Util'
-  }) // end 'models'
+    it('should cast AWSAlreadyPartOfASGError', function (done) {
+      const awsError = new Error('The Instance: i-79600761 is already part of AutoScalingGroup:gamma-dock-pool')
+      awsError.code = 'ValidationError'
+      try {
+        Util.castAWSError(awsError)
+      } catch (err) {
+        expect(err).to.be.an.instanceof(AWSAlreadyPartOfASGError)
+        expect(err.data.originalError).to.equal(awsError)
+        done()
+      }
+    })
+  }) // end'castAWSError
 }) // end 'shiva'
--- test/shiva/unit/tasks/dock.attach.js
+++ test/shiva/unit/tasks/dock.attach.js
@@ -1,87 +1,111 @@
 'use strict'
-
-var Lab = require('lab')
-var lab = exports.lab = Lab.script()
-var describe = lab.describe
-var it = lab.it
-var beforeEach = lab.beforeEach
-var afterEach = lab.afterEach
-var Code = require('code')
-var expect = Code.expect
-var sinon = require('sinon')
+const astralRequire = require('../../../../test/fixtures/astral-require')
+const Code = require('code')
 const joi = require('joi')
-
-var astralRequire = require(
-  process.env.ASTRAL_ROOT + '../test/fixtures/astral-require')
-var loadenv = require('loadenv')
-loadenv.restore()
-loadenv({ project: 'shiva', debugName: 'astral:shiva:test' })
-
-var Promise = require('bluebird')
+const Lab = require('lab')
+const loadenv = require('loadenv')
+const sinon = require('sinon')
+const WorkerStopError = require('error-cat/errors/worker-stop-error')
+require('sinon-as-promised')(Promise)
 
 const AutoScalingGroup = astralRequire('shiva/models/auto-scaling-group')
+const AWSAlreadyPartOfASGError = require('shiva/errors/aws-already-part-of-asg-error')
 const dockAttach = astralRequire('shiva/tasks/dock.attach').task
 const dockAttachSchema = astralRequire('shiva/tasks/dock.attach').jobSchema
+const lab = exports.lab = Lab.script()
+loadenv.restore()
 
-describe('shiva', function () {
-  describe('tasks', function () {
-    describe('dock.attach', function () {
-      beforeEach(function (done) {
-        sinon.stub(AutoScalingGroup, 'attachInstance').returns(Promise.resolve())
+const afterEach = lab.afterEach
+const beforeEach = lab.beforeEach
+const describe = lab.describe
+const expect = Code.expect
+const it = lab.it
+loadenv({ project: 'shiva', debugName: 'astral:shiva:test' })
 
+describe('shiva dock.attach unit test', () => {
+  describe('jobSchema', () => {
+    it('should fatally reject with non-object job', (done) => {
+      joi.validate('no way Jose', dockAttachSchema, (err) => {
+        expect(err).to.be.an.instanceof(Error)
+        expect(err.name).to.match(/ValidationError/)
         done()
       })
+    })
 
-      afterEach(function (done) {
-        AutoScalingGroup.attachInstance.restore()
+    it('should fatally reject without string `githubOrgId`', (done) => {
+      joi.validate({}, dockAttachSchema, (err) => {
+        expect(err).to.be.an.instanceof(Error)
+        expect(err.name).to.match(/ValidationError/)
         done()
       })
+    })
 
-      it('should fatally reject with non-object job', function (done) {
-        joi.validate('no way Jose', dockAttachSchema, (err) => {
-          expect(err).to.be.an.instanceof(Error)
-          expect(err.name).to.match(/ValidationError/)
-          done()
-        })
+    it('should fatally reject with an empty `githubOrgId`', (done) => {
+      joi.validate({ githubOrgId: '' }, dockAttachSchema, (err) => {
+        expect(err).to.be.an.instanceof(Error)
+        expect(err.name).to.match(/ValidationError/)
+        done()
       })
+    })
 
-      it('should fatally reject without string `githubOrgId`', function (done) {
-        joi.validate({}, dockAttachSchema, (err) => {
-          expect(err).to.be.an.instanceof(Error)
-          expect(err.name).to.match(/ValidationError/)
-          done()
-        })
+    it('should fatally reject with non-string `instanceId`', (done) => {
+      let job = { githubOrgId: 'wow', instanceId: 666 }
+      joi.validate(job, dockAttachSchema, (err) => {
+        expect(err).to.be.an.instanceof(Error)
+        expect(err.name).to.match(/ValidationError/)
+        done()
       })
+    })
+  }) // end jobSchema
 
-      it('should fatally reject with an empty `githubOrgId`', function (done) {
-        joi.validate({ githubOrgId: '' }, dockAttachSchema, (err) => {
-          expect(err).to.be.an.instanceof(Error)
-          expect(err.name).to.match(/ValidationError/)
-          done()
-        })
+  describe('task', () => {
+    beforeEach((done) => {
+      sinon.stub(AutoScalingGroup, 'attachInstance').resolves()
+      done()
+    })
+
+    afterEach((done) => {
+      AutoScalingGroup.attachInstance.restore()
+      done()
+    })
+
+    it('should throw worker stop if instance already attached', (done) => {
+      const githubOrgId = 1337
+      const targetASGName = process.env.AWS_AUTO_SCALING_GROUP_PREFIX + githubOrgId
+      const instanceId = 'i-4f4nt45y'
+      const testError = new Error(`The Instance: ${instanceId} is already part of AutoScalingGroup:${targetASGName}`)
+      const job = { githubOrgId: githubOrgId, instanceId: instanceId }
+      AutoScalingGroup.attachInstance.rejects(new AWSAlreadyPartOfASGError(testError))
+      dockAttach(job).asCallback((err) => {
+        expect(err).to.be.an.instanceof(WorkerStopError)
+        done()
       })
+    })
 
-      it('should fatally reject with non-string `instanceId`', function (done) {
-        let job = { githubOrgId: 'wow', instanceId: 666 }
-        joi.validate(job, dockAttachSchema, (err) => {
-          expect(err).to.be.an.instanceof(Error)
-          expect(err.name).to.match(/ValidationError/)
-          done()
-        })
+    it('should throw original error instance already attached', (done) => {
+      const githubOrgId = 1337
+      const targetASGName = process.env.AWS_AUTO_SCALING_GROUP_PREFIX + githubOrgId
+      const instanceId = 'i-4f4nt45y'
+      const testError = new Error(`The Instance: ${instanceId} is already part of AutoScalingGroup:${targetASGName}`)
+      const job = { githubOrgId: githubOrgId, instanceId: instanceId }
+      AutoScalingGroup.attachInstance.rejects(testError)
+      dockAttach(job).asCallback((err) => {
+        expect(err).to.not.be.an.instanceof(WorkerStopError)
+        done()
       })
+    })
 
-      it('should call AutoScalingGroup.attachInstances', function (done) {
-        let githubOrgId = 1337
-        let instanceId = 'i-4f4nt45y'
-        var job = { githubOrgId: githubOrgId, instanceId: instanceId }
-        dockAttach(job).asCallback(function (err) {
-          expect(err).to.not.exist()
-          expect(AutoScalingGroup.attachInstance.calledOnce).to.be.true()
-          expect(AutoScalingGroup.attachInstance.firstCall.args[0]).to.equal('testing-' + githubOrgId)
-          expect(AutoScalingGroup.attachInstance.firstCall.args[1]).to.deep.equal([instanceId])
-          done()
-        })
-      }) // end 'dock.attach'
-    }) // end 'asg.update'
-  }) // end 'tasks'
+    it('should call AutoScalingGroup.attachInstances', (done) => {
+      let githubOrgId = 1337
+      let instanceId = 'i-4f4nt45y'
+      const job = { githubOrgId: githubOrgId, instanceId: instanceId }
+      dockAttach(job).asCallback((err) => {
+        expect(err).to.not.exist()
+        expect(AutoScalingGroup.attachInstance.calledOnce).to.be.true()
+        expect(AutoScalingGroup.attachInstance.firstCall.args[0]).to.equal('testing-' + githubOrgId)
+        expect(AutoScalingGroup.attachInstance.firstCall.args[1]).to.deep.equal([instanceId])
+        done()
+      })
+    })
+  }) // end 'task'
 }) // end 'shiva'
