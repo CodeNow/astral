a991f9d52a69d8d23bbd2e305efd9f7d81d7d7be
Henry Mollman <henry@runnable.com>
2017-05-25 15:30:35 -0700

    Add user vault

--- lib/shiva/scripts/aws-instance-user-data.sh
+++ lib/shiva/scripts/aws-instance-user-data.sh
@@ -6,5 +6,8 @@ DOCK_INIT_SCRIPT=/opt/runnable/dock-init/init.sh
 CONSUL_HOSTNAME={{consul_hostname}}
 export CONSUL_HOSTNAME
 
+USER_VAULT_HOSTNAME={{user_vault_hostname}}
+export USER_VAULT_HOSTNAME
+
 # Initialize the dock
 bash $DOCK_INIT_SCRIPT >> /var/log/user-script-dock-init.log 2>&1
