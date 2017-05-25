19392dd0e5d33bc9553705672997284a0c65295a
Henry Mollman <henry@runnable.com>
2017-05-25 16:05:32 -0700

    Wrong Var!

--- lib/shiva/scripts/aws-instance-user-data.sh
+++ lib/shiva/scripts/aws-instance-user-data.sh
@@ -6,7 +6,7 @@ DOCK_INIT_SCRIPT=/opt/runnable/dock-init/init.sh
 CONSUL_HOSTNAME={{consul_hostname}}
 export CONSUL_HOSTNAME
 
-USER_VAULT_HOSTNAME={{user_vault_hostname}}
+USER_VAULT_HOSTNAME={{user_vault_load_balancer}}
 export USER_VAULT_HOSTNAME
 
 # Initialize the dock
